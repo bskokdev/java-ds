24162e134114301e97f390c2e5fc453fa3ca32c8
Boris Skok <skokcmd@gmail.com>
2023-07-25 15:28:01 +0200

    update DSU impl

--- src/main/java/DSU.java
+++ src/main/java/DSU.java
@@ -1,21 +1,23 @@
 public class DSU {
   private final int[] parent;
-  private final int[] size;
+  private final int[] sizeRank;
 
   public DSU(int size) {
     this.parent = new int[size];
-    this.size = new int[size];
+    this.sizeRank = new int[size];
   }
 
   public void makeSet(int v) {
     parent[v] = v;
-    size[v] = 1;
+    sizeRank[v] = 1;
   }
 
   public int findSet(int v) {
+    // return representative node of the set
     if (parent[v] == v) {
       return v;
     }
+    // Path compression -> we directly append nodes to the root
     return parent[v] = findSet(parent[v]);
   }
 
@@ -23,14 +25,18 @@ public class DSU {
     a = findSet(a);
     b = findSet(b);
     if (a != b) {
-      // we attach the smaller tree to the bigger one
-      if (size[a] < size[b]) {
+      // we want to attach a smaller ranked tree to the bigger ranked one
+      if (sizeRank[a] < sizeRank[b]) {
         int temp = a;
         a = b;
         b = temp;
       }
+      // 'a' is guaranteed here to be a tree with bigger rank
+      // set 'a' representative node to be the parent of 'b' representative
+      // this will set 'b' tree to be a subtree of 'a' tree (set)
+      // also add the 'b' rank to the 'a' rank as the sets merge
       parent[b] = a;
-      size[a] += size[b];
+      sizeRank[a] += sizeRank[b];
     }
   }
 }
